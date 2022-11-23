ba9057ff3b9c8553d47e672f0fe2e973db14a113
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2022-11-23 12:21:34 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+let currfreq = 0
+let lastfreq = 0
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(127)
+})
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(0)
+})
+basic.forever(function () {
+    currfreq = Math.map(input.acceleration(Dimension.X), -1024, 1023, 1, 5000)
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Sine,
+    Math.map(input.acceleration(Dimension.X), -1024, 1023, 1, 5000),
+    Math.map(input.acceleration(Dimension.Y), -1024, 1023, 1, 5000),
+    randint(0, 1024),
+    randint(0, 1024),
+    randint(40, 100),
+    SoundExpressionEffect.Vibrato,
+    InterpolationCurve.Curve
+    ), SoundExpressionPlayMode.UntilDone)
+    lastfreq = currfreq
+})
